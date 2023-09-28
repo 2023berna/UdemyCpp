25da0a334093198598f9dd3f0ec85b2b35059aa0
2023berna <berna.kurt@outlook.de>
2023-09-28 09:38:23 +0200

    Boolean11to12

--- 02_Basics/2_2/Boolean.cc
+++ 02_Basics/2_2/Boolean.cc
@@ -9,7 +9,7 @@
 
 int main()
 {
-    std::int32_t number = 11;
+    std::int32_t number = 12;
 
     bool check = !((number >= 0) && (number <= 10));
     std::cout << "Check: " << std::boolalpha << check << std::endl;
